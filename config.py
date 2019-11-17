9d6e2e7812df7d656538fd478100f667e30a6eba
esaki01 <social0430@gmail.com>
2019-11-17 15:48:04 +0900

    Refactoring

--- config.py
+++ config.py
@@ -1,6 +1,24 @@
-class DevelopmentConfig:
+class Config:
+    """Base configuration."""
+
+
+class ProdConfig(Config):
+    """Production configuration."""
+
+    ENV = "production"
+    DEBUG = False
+
+
+class DevConfig(Config):
+    """Development configuration."""
+
+    ENV = "development"
     DEBUG = True
 
 
-class ProductionConfig:
-    DEBUG = False
+class TestConfig(Config):
+    """Test configuration."""
+
+    ENV = "test"
+    TESTING = True
+    DEBUG = True
